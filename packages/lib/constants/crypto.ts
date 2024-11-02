7fdbfbdce867b735f4e570c92be620ecae0e7931
Hugues Ebert M. <huguesbert17@gmail.com>
2024-11-01 23:55:32 -0400

    Update crypto.ts
    Renaming two exported constants in the packages/lib/constants/crypto.ts file from DOCUMENSO_ENCRYPTION_KEY and DOCUMENSO_ENCRYPTION_SECONDARY_KEY to ENCRYPTION_KEY and ENCRYPTION_SECONDARY_KEY

--- packages/lib/constants/crypto.ts
+++ packages/lib/constants/crypto.ts
@@ -1,20 +1,20 @@
-export const DOCUMENSO_ENCRYPTION_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_KEY;
+export const ENCRYPTION_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_KEY;
 
-export const DOCUMENSO_ENCRYPTION_SECONDARY_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY;
+export const ENCRYPTION_SECONDARY_KEY = process.env.NEXT_PRIVATE_ENCRYPTION_SECONDARY_KEY;
 
 if (typeof window === 'undefined') {
-  if (!DOCUMENSO_ENCRYPTION_KEY || !DOCUMENSO_ENCRYPTION_SECONDARY_KEY) {
-    throw new Error('Missing DOCUMENSO_ENCRYPTION_KEY or DOCUMENSO_ENCRYPTION_SECONDARY_KEY keys');
+  if (!ENCRYPTION_KEY || !ENCRYPTION_SECONDARY_KEY) {
+    throw new Error('Missing ENCRYPTION_KEY or ENCRYPTION_SECONDARY_KEY keys');
   }
 
-  if (DOCUMENSO_ENCRYPTION_KEY === DOCUMENSO_ENCRYPTION_SECONDARY_KEY) {
+  if (ENCRYPTION_KEY === ENCRYPTION_SECONDARY_KEY) {
     throw new Error(
-      'DOCUMENSO_ENCRYPTION_KEY and DOCUMENSO_ENCRYPTION_SECONDARY_KEY cannot be equal',
+      'ENCRYPTION_KEY and ENCRYPTION_SECONDARY_KEY cannot be equal',
     );
   }
 }
 
-if (DOCUMENSO_ENCRYPTION_KEY === 'CAFEBABE') {
+if (ENCRYPTION_KEY === 'CAFEBABE') {
   console.warn('*********************************************************************');
   console.warn('*');
   console.warn('*');
